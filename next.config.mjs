bcb6e1857b089f1b81a6b045068db4a7515de08d
Aziz Morsly <aziz.morsly@gmail.com>
2024-09-21 14:54:15 +0200

    Add public directory and update configuration for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -9,10 +9,10 @@ const nextConfig = {
     ],
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  output: 'standalone',
+  distDir: '.next',
 }
 
 export default nextConfig;
